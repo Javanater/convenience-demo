8ae9b528f5d4ec621017ee54c67c8d9ccd89bc51
R. Madison Fortenberry <fortenberry.rm@protonmail.com>
2019-08-02 13:45:47 -0400

    no submodule

---
+++ demo.cpp
@@ -0,0 +1,16 @@
+#include "using_convenience.hpp"
+
+int main()
+{
+    cout << "wow, such convenience!!" << endl;
+
+    for (auto e : path_iterator(".") | indexed())
+        cout << "%02d"_f % e.index() << e.value() << endl;
+
+    vector<int> v;
+    v += 3,5,9;
+
+    for_each(v, cout << _1 << endl);
+
+    cout << steady_clock::now() << endl;
+}
